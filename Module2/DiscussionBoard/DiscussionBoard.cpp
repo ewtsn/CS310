88c45e338729f8ba7fc2dbb6105a307122df7a5d
Eric Watson <eriwatson@my365.bellevue.edu>
2024-09-05 09:53:38 -0500

    Module 2 Discussion Board

---
+++ Module2/DiscussionBoard/DiscussionBoard.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int num1;
+    string num2;
+
+    cout << "Please input a number" << endl;
+    cin >> num1;
+
+    cout << "Please input a different number" << endl;
+    cin >> num2;
+
+    cout << "Your numbers add to: " << (num1 + num2) << "!" << end;
+
+    return 0;
+}
