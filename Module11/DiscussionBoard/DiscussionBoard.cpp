2ad7e7abc5dd37b37990117e28cfe193f0ae6745
Eric Watson <eriwatson@my365.bellevue.edu>
2024-11-07 20:31:04 -0600

    Forgot mistakes

--- Module11/DiscussionBoard/DiscussionBoard.cpp
+++ Module11/DiscussionBoard/DiscussionBoard.cpp
@@ -4,11 +4,11 @@
 using namespace std;
 
 template <typename T> string type(T x) {
-    return typeid(x).name();
+    return typeid(x).name;
 }
 
 int main() {
-    string s = "Hello!";
+    string "Hello!";
     int i = 10;
 
     cout << "Here is the output of tyle() for a string: " << type(s) << endl;
